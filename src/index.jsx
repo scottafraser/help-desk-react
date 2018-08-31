63661d6cfe24e95e32c604d1e4e050e0dc440a3e
Scott Fraser <scottafraser@yahoo.com>
2018-08-31 14:53:32 -0700

    inline styling, webpack scripts installed

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { AppContainer } from 'react-hot-loader';
 
-
-ReactDOM.render(
-  <App/>,
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
     document.getElementById('react-app-root')
-);
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App)
+  });
+}
