53773125f224da5a831ea6ce48ec50d96338975c
Scott Fraser <scottafraser@yahoo.com>
2018-08-31 14:20:55 -0700

    local hosting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList";
+
 
 function App() {
     return (
         <div>
-            <h1>Help Queue</h1>
-            <h3>3a</h3>
-            <h3>Thato and Haley</h3>
-            <p><em>Firebase entries not saving!</em></p>
-            <hr />
+            <Header/>
+            <TicketList/>
         </div>
     );
 }
